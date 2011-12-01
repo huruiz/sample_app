dc57f29c800790b639b329ed66e0cee3c579563f
Hugo Ruiz <huruiz@hotmail.com>
2011-12-01 13:13:12 -0600

    Added user microposts

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,3 +1,5 @@
+ 
+
 Factory.define :user do |user|
   user.name                  "Michael Hartl"
   user.email                 "mhartl@example.com"
@@ -5,11 +7,17 @@ Factory.define :user do |user|
   user.password_confirmation "foobar"
 end
 
+Factory.sequence :email do |n|
+  "person-#{n}@example.com"
+end
+
 Factory.sequence :name do |n|
   "Person #{n}"
 end
 
-Factory.sequence :email do |n|
-  "person-#{n}@example.com"
-end
+Factory.define :micropost do |micropost|
+  micropost.content "Foo bar"
+  micropost.association :user
+end	 
+
 
